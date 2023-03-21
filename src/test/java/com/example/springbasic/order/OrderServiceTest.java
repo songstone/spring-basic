806941ffe85d67d73fda8afb783dbe101ea8bd0f
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-03-21 22:15:23 +0900

    test: member, order test

---
+++ src/test/java/com/example/springbasic/order/OrderServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.springbasic.order;
+
+import com.example.springbasic.member.Grade;
+import com.example.springbasic.member.Member;
+import com.example.springbasic.member.MemberService;
+import com.example.springbasic.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
