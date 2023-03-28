72e1abf37ab502b3c302d2c6c8de0246dcf6bd97
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-03-28 23:53:42 +0900

    test: add test

---
+++ src/test/java/com/example/springbasic/autowired/AllBeanTest.java
@@ -0,0 +1,49 @@
+package com.example.springbasic.autowired;
+
+import com.example.springbasic.AutoAppConfig;
+import com.example.springbasic.discount.DiscountPolicy;
+import com.example.springbasic.member.Grade;
+import com.example.springbasic.member.Member;
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AllBeanTest {
+
+    @Test
+    void finaAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(
+            AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "rateDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
+}
