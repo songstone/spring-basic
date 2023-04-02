1f5d988316d72268fc1ef9f8548a62e6351bce0f
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-04-02 20:08:41 +0900

    feat: add web dependency

---
+++ src/main/java/com/example/springbasic/common/MyLogger.java
@@ -0,0 +1,36 @@
+package com.example.springbasic.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("MyLogger.init " + "[" + uuid + "] request scope benn create : "  + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("MyLogger.close " + "[" + uuid + "] request scope benn close : "  + this);
+    }
+}
